54ee0ebd37fea9a342d03696f3fe5e9ac4ce1358
Magic <tramwajek@gmail.com>
2024-08-21 12:36:29 +0200

    add try catch to validateToken

--- src/APIcalls/AuthCalls.tsx
+++ src/APIcalls/AuthCalls.tsx
@@ -2,14 +2,18 @@ export const validateToken = async () => {
     console.log("validating token");
     if (localStorage.getItem("token")) {
         const token = localStorage.getItem("token");
-        const response = await fetch('https://localhost:5001/User/ValidateToken', {
-            method: "GET",
-            headers: {
-                "Authorization": `Bearer ${token}`
+        try {
+            const response = await fetch('https://localhost:5001/User/ValidateToken', {
+                method: "GET",
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                }
+            });
+            if (response.ok) {
+                return true;
             }
-        });
-        if (response.ok) {
-            return true;
+        } catch (error) {
+            console.error('Error validating token:', error);
         }
         return null;
         // if (token && user) {
