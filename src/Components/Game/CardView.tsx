16120c40634d27608d7f1bc95efad9bd36ce40a2
Magic <tramwajek@gmail.com>
2024-08-21 12:37:18 +0200

    update CardView (very raw)

--- src/Components/Game/CardView.tsx
+++ src/Components/Game/CardView.tsx
@@ -1,23 +1,59 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./CardView.css";
 import { Card, ColourEnum } from "./GameTypes";
+import { ReactComponent as Crown } from "./whiteCrown.svg";
+import { Button } from "react-bootstrap";
 
-const CardView: React.FC<{ cardProps: Card }> = (props) => {
-    const cardProps = props.cardProps;
+//todo: insert the smaller "coin" (as on CoinBoard) in the card cost
+const CardView: React.FC<{ cardProps: Card, selectCard: (cardId: number | undefined) => void }> = (props) => {
+    const [card, setCard] = React.useState<Card | null>(null);
+    const [cardColour, setCardColour] = React.useState<ColourEnum>(ColourEnum.White);
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        if (card !== null && card.id !== undefined)
+            props.selectCard(card?.id);
+    };
+    const fontColour = (colour: ColourEnum) => {
+        switch (colour.toString()) {
+            case "Black":
+                return "White";
+            default:
+                return "Black";
+        }
+    }
+    const cardDisplayColour = (colour: ColourEnum) => {
+        switch (colour.toString()) {
+            case "Multi":
+                return "Pink";
+            default:
+                return colour.toString();
+        }
+    };
+    useEffect(() => {
+        setCard(props.cardProps);
+        setCardColour(props.cardProps.colour);
+        console.log("CardView mounted");
+        if (card) console.log("cardProps:", card);
+    }, [props.cardProps]);
     return (
-        <div className="card-container box">
-            <div className="top" style={{ backgroundColor: ColourEnum[cardProps.colour] }}>
-                <div className="points">{cardProps.points}</div>
-                <div className="crowns">{cardProps.crowns}</div>
-                <div className="miningPower">{cardProps.miningPower}</div>
+        { card } && <button className="card-container box" onClick={handleClick}>
+            <div className="top" style={{ backgroundColor: cardDisplayColour(cardColour), color: fontColour(cardColour) }}>
+                <div className="points">{card?.points}</div>
+                <div className="crowns">
+                    {card?.crowns != undefined && card?.crowns > 0 ? 
+                    Array.from(Array(card?.crowns), (_, i) => 
+                        <Crown key={i}/>) : ""
+                        }
+                </div>
+                <div className="miningPower" >{card?.miningPower}</div>
             </div>
             <div className="bot">
-                {cardProps.cardCost?.map((x) =>
-                    <div className="dot" style={{ backgroundColor: ColourEnum[x[0]] }}>
-                        {x[1]}
+                {card?.cardCost?.map((x) =>
+                    <div className="dot" key={x.colour} style={{ backgroundColor: x.colour.toString() }} >
+                        <div style={{ color: fontColour(x.colour) }}>{x.amount}</div>
                     </div>)}
             </div>
-        </div>
+        </button>
     );
+
 }
 export default CardView;
