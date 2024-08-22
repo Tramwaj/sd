bfe94a6523c14e52ff287e967b36c8652d529981
Magic <tramwajek@gmail.com>
2024-08-22 15:08:07 +0200

    format raw PlayerBoardsView

--- src/Components/Game/PlayerBoardsView.tsx
+++ src/Components/Game/PlayerBoardsView.tsx
@@ -57,9 +57,42 @@ const PlayerBoardsView: React.FC<{ player1board: PlayerBoard, player2Board: Play
         <div className="playerBoards">
             {player1Board ? (
                 <div className="playerBoard">
+                    <div className="totals">
+                        <div className="points">{player1Board.totalPoints} pkt</div>
+                        <div className="scrolls">{player1Board.scrollsCount} scrolls</div>
+                        <div className="crowns">{player1Board.crowns} crowns</div>
+                    </div>
                     {colours.map(colour => (
-                        <div className="colour" key={colour} style={{backgroundColor:colour.toString(), color:fontColour(colour)}}>
-                            <div className="points">{player1Board.pointsByColour.get(colour)??0} pkt</div>
+                        <div className="colour" key={colour} style={{ backgroundColor: colour.toString(), color: fontColour(colour) }}>
+                            <div className="miningPower">
+                                {player1Board.miningValues?.get(colour) ?
+                                Array.from(Array(player1Board.miningValues?.get(colour)), (_, i) =>
+                                    <div className="rect" key={i} style={{ backgroundColor: fontColour(colour) }}></div>)
+                                : ""}
+                            </div>
+                            <div className="coins">
+                                {player1Board.coins?.get(colour) ? 
+                                Array.from(Array(player1Board.coins?.get(colour)), (_, i) =>
+                                    <div className="coinDot" key={i} style={{ backgroundColor: fontColour(colour) }}></div>)
+                                : ""}
+                            </div>
+                            <div className="points">{player1Board.pointsByColour.get(colour) ?? 0} p</div>
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <div>Loading...</div>
+            )}
+            {player1Board ? (
+                <div className="playerBoard">
+                    <div className="totals">
+                        <div className="points">{player1Board.totalPoints} pkt</div>
+                        <div className="scrolls">{player1Board.scrollsCount} scrolls</div>
+                        <div className="crowns">{player1Board.crowns} crowns</div>
+                    </div>
+                    {colours.map(colour => (
+                        <div className="colour" key={colour} style={{ backgroundColor: colour.toString(), color: fontColour(colour) }}>
+                            <div className="points">{player1Board.pointsByColour.get(colour) ?? 0} pkt</div>
                             <div className="miningPower">{player1Board.miningValues.get(colour)}</div>
                             <div className="coins">{player1Board.coins.get(colour)}</div>
                         </div>
@@ -68,7 +101,6 @@ const PlayerBoardsView: React.FC<{ player1board: PlayerBoard, player2Board: Play
             ) : (
                 <div>Loading...</div>
             )}
-            <div className="playerBoard">Player 2</div>
         </div>
     );
 }
