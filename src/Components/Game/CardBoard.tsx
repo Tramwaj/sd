dbafb72f71b346747b5e6edf2ce7568c4fb2db1a
Magic <tramwajek@gmail.com>
2024-08-29 14:21:05 +0200

    remove fake data from CardBoard

--- src/Components/Game/CardBoard.tsx
+++ src/Components/Game/CardBoard.tsx
@@ -18,32 +18,7 @@ const CardBoard: React.FC<{cardsProps:Cards}> = (props) => {
         console.log('cards:', cards);
         console.log('cardsProps:', props.cardsProps);
     }, []);
-    const cost: SingleCost[] = [
-        { colour: ColourEnum.Red, amount: 1 },
-        { colour: ColourEnum.Blue, amount: 2 },
-        { colour: ColourEnum.Green, amount: 3 }
-    ];
-    const card:Card = {
-        id: 1,
-        colour: ColourEnum.Red,
-        crowns: 2,
-        miningPower: 1,
-        points: 1,
-        cardCost: cost,
-    }
     
-    const CardLevel1 = {
-        exposed: [card,card,card,card,card],
-        deckCount: 3,
-    }
-    const CardLevel2 = {
-        exposed: [card,card,card,card],
-        deckCount: 4,
-    }
-    const CardLevel3 = {
-        exposed: [card,card,card],
-        deckCount: 5,
-    }
     return (
         <div>
             {(cards?.level3) &&<div>
