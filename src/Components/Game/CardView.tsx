3a713edc5b310da6d56dd0545b8bed6ac78a893f
Magic <tramwajek@gmail.com>
2024-09-10 18:20:11 +0200

    display card.Action

--- src/Components/Game/CardView.tsx
+++ src/Components/Game/CardView.tsx
@@ -6,11 +6,11 @@ import { Button } from "react-bootstrap";
 import { cardDisplayColour, fontColour } from "../Globals/StyleFunctions";
 
 //todo: insert the smaller "coin" (as on CoinBoard) in the card cost
-const CardView: React.FC<{ cardProps: Card, selectCard: (action:Action) => void }> = (props) => {
+const CardView: React.FC<{ cardProps: Card, selectCard: (action: Action) => void }> = (props) => {
     const [card, setCard] = React.useState<Card | null>(null);
     const [cardColour, setCardColour] = React.useState<ColourEnum>(ColourEnum.White);
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        if (card !== null && card.id !== undefined){
+        if (card !== null && card.id !== undefined) {
             const cardRequest: BuyCardRequest = {
                 cardId: card.id,
                 colour: card.colour, //todo: ask for card coolour when colour == multi
@@ -22,7 +22,7 @@ const CardView: React.FC<{ cardProps: Card, selectCard: (action:Action) => void 
             props.selectCard(action);
         }
     };
-    
+
     useEffect(() => {
         setCard(props.cardProps);
         setCardColour(props.cardProps.colour);
@@ -34,18 +34,23 @@ const CardView: React.FC<{ cardProps: Card, selectCard: (action:Action) => void 
             <div className="top" style={{ backgroundColor: cardDisplayColour(cardColour), color: fontColour(cardColour) }}>
                 <div className="points">{card?.points}</div>
                 <div className="crowns">
-                    {card?.crowns != undefined && card?.crowns > 0 ? 
-                    Array.from(Array(card?.crowns), (_, i) => 
-                        <Crown key={i}/>) : ""
-                        }
+                    {card?.crowns != undefined && card?.crowns > 0 ?
+                        Array.from(Array(card?.crowns), (_, i) =>
+                            <Crown key={i} />) : ""
+                    }
                 </div>
                 <div className="miningPower" >{card?.miningPower}</div>
             </div>
             <div className="bot">
-                {card?.cardCost?.map((x) =>
-                    <div className="dot" key={x.colour} style={{ backgroundColor: x.colour.toString() }} >
-                        <div style={{ color: fontColour(x.colour) }}>{x.amount}</div>
-                    </div>)}
+                <div className="dots">
+                    {card?.cardCost?.map((x) =>
+                        <div className="dot" key={x.colour} style={{ backgroundColor: x.colour.toString() }} >
+                            <div style={{ color: fontColour(x.colour) }}>{x.amount}</div>
+                        </div>)}
+                </div>
+                <div className="action">
+                    {card?.action}
+                </div>
             </div>
         </button>
     );
