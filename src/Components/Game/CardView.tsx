becd9e7e1bc81e076d2abd5cae30181788adaa17
Magic <tramwajek@gmail.com>
2024-09-12 14:35:46 +0200

    add multi-colour cards buying

--- src/Components/Game/CardView.tsx
+++ src/Components/Game/CardView.tsx
@@ -4,17 +4,40 @@ import { Action, ActionType, BuyCardRequest, Card, ColourEnum } from "./GameType
 import { ReactComponent as Crown } from "./whiteCrown.svg";
 import { Button } from "react-bootstrap";
 import { cardDisplayColour, fontColour } from "../Globals/StyleFunctions";
+import { ChooseColorModal } from "./ChooseColorModal";
 
 //todo: insert the smaller "coin" (as on CoinBoard) in the card cost
 const CardView: React.FC<{ cardProps: Card, selectCard: (action: Action) => void }> = (props) => {
     const [card, setCard] = React.useState<Card | null>(null);
     const [cardColour, setCardColour] = React.useState<ColourEnum>(ColourEnum.White);
-    
+    const [isChooseColorModalOpen, setIsChooseColorModalOpen] = React.useState<boolean>(false);
+    const [isCardClickable, setIsCardClickable] = React.useState<boolean>(true);
+
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
         if (card !== null && card.id !== undefined) {
+            if (card.colour === ColourEnum.Multi) {
+                setIsChooseColorModalOpen(true);
+                return;
+            }
             const cardRequest: BuyCardRequest = {
                 cardId: card.id,
                 colour: card.colour, //todo: ask for card coolour when colour == multi
+            }
+            const action = {
+                type: ActionType.BuyCard,
+                payload: cardRequest,
+            }
+            props.selectCard(action);
+        }
+    };
+    const handleColorModalClose = () => {
+        setIsChooseColorModalOpen(false);
+    }
+    const sendMultiColouredCard = (colour: ColourEnum) => {
+        if (card !== null && card.id !== undefined) {
+            const cardRequest: BuyCardRequest = {
+                cardId: card.id,
+                colour: colour,
             }
             const action = {
                 type: ActionType.BuyCard,
@@ -29,31 +52,35 @@ const CardView: React.FC<{ cardProps: Card, selectCard: (action: Action) => void
         setCardColour(props.cardProps.colour);
         //console.log("CardView mounted");
         //if (card) console.log("cardProps:", card);
-    }, [props.cardProps, card]);
+    }, [props.cardProps, card, isChooseColorModalOpen]);
     return (
-        { card } && <button className="card-container box" onClick={handleClick}>
-            <div className="top" style={{ backgroundColor: cardDisplayColour(cardColour), color: fontColour(cardColour) }}>
-                <div className="points">{card?.points}</div>
-                <div className="crowns">
-                    {card?.crowns != undefined && card?.crowns > 0 ?
-                        Array.from(Array(card?.crowns), (_, i) =>
-                            <Crown key={i} />) : ""
-                    }
+        <div>
+            {card && <button className="card-container box" onClick={handleClick}>
+                <div className="top" style={{ backgroundColor: cardDisplayColour(cardColour), color: fontColour(cardColour) }}>
+                    <div className="points">{card?.points}</div>
+                    <div className="crowns">
+                        {card?.crowns != undefined && card?.crowns > 0 ?
+                            Array.from(Array(card?.crowns), (_, i) =>
+                                <Crown key={i} />) : ""
+                        }
+                    </div>
+                    <div className="miningPower" >{card?.miningPower}</div>
                 </div>
-                <div className="miningPower" >{card?.miningPower}</div>
-            </div>
-            <div className="bot">
-                <div className="dots">
-                    {card?.cardCost?.map((x) =>
-                        <div className="dot" key={x.colour} style={{ backgroundColor: x.colour.toString() }} >
-                            <div style={{ color: fontColour(x.colour) }}>{x.amount}</div>
-                        </div>)}
+                <div className="bot">
+                    <div className="dots">
+                        {card?.cardCost?.map((x) =>
+                            <div className="dot" key={x.colour} style={{ backgroundColor: x.colour.toString() }} >
+                                <div style={{ color: fontColour(x.colour) }}>{x.amount}</div>
+                            </div>)}
+                    </div>
+                    <div className="action">
+                        {card?.action}
+                    </div>
                 </div>
-                <div className="action">
-                    {card?.action}
-                </div>
-            </div>
-        </button>
+            </button>}
+            { isChooseColorModalOpen && <ChooseColorModal show={isChooseColorModalOpen} handleClose={() => setIsChooseColorModalOpen(false)} sendColour={sendMultiColouredCard} />}
+        </div>
+        
     );
 
 }
---
+++ src/Components/Game/ChooseColorModal.tsx
@@ -0,0 +1,48 @@
+import { useEffect } from "react";
+import { ActionType, ColourEnum } from "./GameTypes";
+import { Button, Modal } from "react-bootstrap";
+import React from "react";
+
+export const ChooseColorModal: React.FC<{ show: boolean, handleClose: () => void, sendColour: (colour: ColourEnum) => void }> = (props) => {
+    const [show, setShow] = React.useState<boolean>(false);
+    //const [selectedColour, setSelectedColour] = React.useState<ColourEnum>(ColourEnum.Black);
+    const [colours, setColours] = React.useState<ColourEnum[]>([]);
+    useEffect(() => {
+        setShow(props.show);
+        const colours = [ColourEnum.White, ColourEnum.Blue, ColourEnum.Green, ColourEnum.Red, ColourEnum.Black];
+        setColours(colours);
+    }, [props.show]);
+    const handleColourSelect = (event: React.MouseEvent<HTMLButtonElement>) => {
+        let colour = event.currentTarget.style.backgroundColor;
+        colour = colour[0].toUpperCase() + colour.substring(1);
+        //setSelectedColour(colour as ColourEnum);
+        const colourstr = colour as keyof typeof ColourEnum;
+        const colourEnum = ColourEnum[colourstr];
+        props.sendColour(colourEnum);
+        props.handleClose();
+    }
+
+return (
+    <Modal show={show} onHide={props.handleClose}>
+        <Modal.Header closeButton>
+            <Modal.Title>Choose colour</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <div className="colourSelect">
+                {colours.map((colour) => (
+                    <button key={colour} onClick={handleColourSelect} style={{ backgroundColor: colour.toString() }}>
+                    </button>
+                ))}
+            </div>
+        </Modal.Body>
+        <Modal.Footer>
+            <Button variant="secondary" onClick={props.handleClose}>
+                Close
+            </Button>
+            <Button variant="primary" onClick={props.handleClose}>
+                Select
+            </Button>
+        </Modal.Footer>
+    </Modal>
+);
+}
